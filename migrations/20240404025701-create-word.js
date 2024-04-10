a7db28d54946aa849b12c8c1f6a4f8df76afcf08
Andrew <andrew@andrewsmith.org>
2024-04-10 19:50:05 -0400

    Change model type to accommodate > 255 chars

--- migrations/20240404025701-create-word.js
+++ migrations/20240404025701-create-word.js
@@ -1,31 +1,31 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('words', {
+    await queryInterface.createTable("words", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       word: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT,
       },
       definition: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('words');
-  }
+    await queryInterface.dropTable("words");
+  },
 };
--- models/word.js
+++ models/word.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class word extends Model {
     /**
@@ -13,12 +11,15 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  word.init({
-    word: DataTypes.STRING,
-    definition: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'word',
-  });
+  word.init(
+    {
+      word: DataTypes.TEXT,
+      definition: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "word",
+    }
+  );
   return word;
 };
